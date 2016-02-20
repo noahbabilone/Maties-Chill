099cd342f281336497206e761e537202b52e7d7d
yann <noahbabilone@gmail.com>
2016-02-20 18:29:20 +0100

    Added, configured and overload FOSUserBundle for UserBundle

--- src/MCBundle/Controller/FilmController.php
+++ src/MCBundle/Controller/FilmController.php
@@ -1,13 +1,66 @@
 <?php
 
 namespace MCBundle\Controller;
-
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class FilmController extends Controller
 {
     public function indexAction()
     {
         return $this->render('MCBundle:Default:index.html.twig');
+    }
+    
+    public function filmAllAction(){
+        $em = $this->getDoctrine()->getManager();
+        $result = $em->getRepository('MCBundle:Film')->findAll();
+       $dataFilm = array();
+        foreach ($result as $film){ 
+            
+            $data = array();
+            $data['id']=$film->getID();
+            $data['ISAN']=$film->getISAN();
+            $data['title']=$film->getTitle();
+            $data['releaseDate']=$film->getReleaseDate();
+            $data['directors']=$film->getDirectors();
+            $data['actors']=$film->getActors();
+            $data['duration']=$film->getDuration();
+            $data['ageLimit']=$film->getAgeLimit();
+            $data['pressRating']=$film->getPressRating();
+            $data['userRating']=$film->getUserRating();
+            $data['link']=$film->getLink();
+            $data['description']=$film->getDescription();
+            $data['category']=$film->getCategory()->getTitle();
+            
+            //var_dump($data);
+            $dataFilm[]=$data;
+            
+        }
+        return new JSONResponse($dataFilm);
+    }
+    
+     public function filmAllsAction(){
+        $em = $this->getDoctrine()->getManager();
+        $result = $em->getRepository('MCBundle:Film')->findAll();
+        $dataFilm = array();
+        foreach ($result as $film){ 
+            $data = array();
+            $data['id']=$film->getID();
+            $data['ISAN']=$film->getISAN();
+            $data['title']=$film->getTitle();
+            $data['releaseDate']=$film->getReleaseDate();
+            $data['directors']=$film->getDirectors();
+            $data['actors']=$film->getActors();
+            $data['duration']=$film->getDuration();
+            $data['ageLimit']=$film->getAgeLimit();
+            $data['pressRating']=$film->getPressRating();
+            $data['userRating']=$film->getUserRating();
+            $data['link']=$film->getLink();
+            $data['description']=$film->getDescription();
+            $data['category']=$film->getCategory()->getTitle();
+            $dataFilm[]=$data;
+            
+        }
+        return $dataFilm;
     }
 }
---
+++ src/MCBundle/Entity/Category.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace MCBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ *
+ * @ORM\Table(name="category")
+ * @ORM\Entity(repositoryClass="MCBundle\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255, unique=true)
+     */
+    private $title;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Category
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
